fc6a3990abc58689788015ee627eb43565b56b8e
Jamie S <jpesbueno@gmail.com>
2022-12-30 11:58:42 -0500

    Added explanatory comments, and independent tidy dataset with averages across all variables grouped by subject and activity

--- run_analysis.R
+++ run_analysis.R
@@ -11,7 +11,7 @@ y_train_path <- "./UCI HAR Dataset/train/y_train.txt"
 subj_train_path <- "./UCI HAR Dataset/train/subject_train.txt"
 subj_test_path <- "./UCI HAR Dataset/test/subject_test.txt"
 
-
+# Read files into tables where appropriate
 x_test_tbl <- tbl_df(read.table(X_test_path))
 y_test <- tbl_df(read.table(y_test_path))
 x_train_tbl <- tbl_df(read.table(X_train_path))
@@ -21,19 +21,33 @@ activities <- read.table(activities_path)
 subj_train <- read.table(subj_train_path)
 subj_test <- read.table(subj_test_path)
 
+#Combine training and test subject data
 subj <- rbind(subj_train,subj_test)
 
+#Combine test and training observation data
 X_tbl <- rbind(x_train_tbl,x_test_tbl)
+
+#Name columns per features.txt
 colnames(X_tbl) <- features[,2]
+
+#Subset to only mean() and std() variables
 X_pruned_names <- names(X_tbl) %>% grep(pattern="mean|std", value=TRUE)
 X_mean_std <- select(X_tbl, X_pruned_names)
 
+#Combine test and training activity data
 y_tbl <- rbind(y_train,y_test)
+
+#Translate activity numbers to human-readable factors
 y_labeled <- c(1:nrow(y_tbl))
 for(i in 1:nrow(y_tbl)){
   key <- y_tbl[[i,1]]
   val <- activities[[key,2]]
   y_labeled[[i]] <- val
 }
+
+#Combine Subject #, activity factors, observed mean and std variables
 complete_tbl <- cbind(subj,y_labeled,X_mean_std)
-colnames(complete_tbl)[colnames(complete_tbl) %in% c("V1","y_labeled")]<-c("Subject","Actvity")
+colnames(complete_tbl)[colnames(complete_tbl) %in% c("V1","y_labeled")]<-c("Subject","Activity")
+
+# Tidy dataset with the average of each variable grouped by Subject and Activity
+tid_avg <- complete_tbl %>% aggregate(~Subject + Activity, mean(complete_tbl[3:81]))
